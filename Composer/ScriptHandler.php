1a5ac6ea9e49506d6a60c40767d8a9bebf505cfa
Julien Janvier <j.janvier@gmail.com>
2013-03-14 13:46:32 +0100

    Adding a composer script which creates symbolic links for CSS.

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Alazjj\SimpleBootstrapBundle\Composer;
+
+use Composer\Script\Event;
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOException;
+
+class ScriptHandler
+{
+    private static $VENDOR_DIR;
+    private static $BUNDLE_CSS_DIR;
+
+    public function __construct()
+    {
+        if (is_null(self::$VENDOR_DIR))
+        {
+            self::$VENDOR_DIR = __DIR__ . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+        }
+        if (is_null(self::$BUNDLE_CSS_DIR))
+        {
+            self::$BUNDLE_CSS_DIR = __DIR__ . '..' . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR;
+        }
+    }
+
+    public static function buildCssSymlink()
+    {
+        $fs = new Filesystem();
+
+        $finder = new Finder();
+        $finder->files()
+            ->in(self::$VENDOR_DIR)->name('/\.css$/')
+            ->exclude('SimpleBootstrapBundle');
+
+        /** @var $css SplFileInfo */
+        foreach ($finder as $css) {
+            try {
+                $fs->symlink($css->getPath(), self::$VENDOR_DIR . self::$BUNDLE_CSS_DIR . $css->getBasename());
+            } catch (IOException $e) {
+                echo "An error occurred while symlinking the css {$css->getBasename()}.";
+            }
+        }
+    }
+}

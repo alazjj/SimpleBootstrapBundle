279244de3fb83f10314ade6834b116346f7a5920
Julien Janvier <j.janvier@gmail.com>
2013-03-14 15:32:50 +0100

    The post install script now symlinks both CSS and IMAGES.

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -10,52 +10,70 @@ use Symfony\Component\Filesystem\Exception\IOException;
 
 class ScriptHandler
 {
-    private static $VENDOR_DIR;
-    private static $BUNDLE_CSS_DIR;
+    const BUNDLE_NAME = 'SimpleBootstrapBundle';
 
     public static function getVendorDir()
     {
-        if (is_null(self::$VENDOR_DIR))
-        {
-            self::$VENDOR_DIR = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
-        }
-
-        return self::$VENDOR_DIR;
+        return realpath(
+            __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR
+        );
     }
 
     public static function getBundleCssDir()
     {
-        if (is_null(self::$BUNDLE_CSS_DIR))
-        {
-            self::$BUNDLE_CSS_DIR = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR);
-        }
+        return self::getBundleAssetDir('css');
+    }
 
-        return self::$BUNDLE_CSS_DIR;
+    public static function getBundleImgDir()
+    {
+        return self::getBundleAssetDir('img');
+    }
+
+    public static function installAssets()
+    {
+        $cssFinder = new Finder();
+        $cssFinder->files()
+            ->in(self::getVendorDir())
+            ->name('/\.css$/')
+            ->exclude(self::BUNDLE_NAME);
+
+        self::symlinkAssets($cssFinder, 'css');
+
+        $imgFinder = new Finder();
+        $imgFinder->files()
+            ->in(self::getVendorDir())
+            ->name('/\.png$/')
+            ->name('/\.gif$/')
+            ->name('/\.jpg$/')
+            ->name('/\.jpeg$/')
+            ->exclude(self::BUNDLE_NAME);
+
+        self::symlinkAssets($imgFinder, 'img');
     }
 
     /**
-     * Creates a symlink for each CSS into the public/css directory of the bundle.
+     * Creates a symlink for each asset into the public/$assetType directory of the bundle.
      */
-    public static function installAssets()
+    private static function symlinkAssets(Finder $finder, $assetType)
     {
-        $fs = new Filesystem();
+        $getBundleAssetDir = "getBundle" . ucfirst($assetType) . "Dir";
+        $bundleAssetDir    = self::$getBundleAssetDir() . DIRECTORY_SEPARATOR;
+        $fs                = new Filesystem();
 
-        self::getVendorDir();
-        self::getBundleCssDir();
-
-        $finder = new Finder();
-        $finder->files()
-            ->in(self::getVendorDir())
-            ->name('/\.css$/')
-            ->exclude('SimpleBootstrapBundle');
-
-        /** @var $css SplFileInfo */
-        foreach ($finder as $css) {
+        /** @var $asset SplFileInfo */
+        foreach ($finder as $asset) {
             try {
-                $fs->symlink($css->getPathname(), self::getBundleCssDir() . DIRECTORY_SEPARATOR . $css->getBasename());
+                $fs->symlink($asset->getPathname(), $bundleAssetDir . $asset->getBasename());
             } catch (IOException $e) {
-                echo "An error occurred while symlinking the css {$css->getBasename()}.\n";
+                echo "An error occurred while symlinking the asset {$asset->getBasename()}.\n";
             }
         }
+    }
+
+    private static function getBundleAssetDir($assetType)
+    {
+        return realpath(
+            __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . $assetType . DIRECTORY_SEPARATOR
+        );
     }
 }

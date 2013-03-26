64f5986cb667d2b35063e42c88e60c520998cab4
aRn0D <arnaud.langlade@gmail.com>
2013-03-26 23:31:55 +0100

    Add news testes to choice field testes

--- Tests/Form/Extension/ChoiceTypeTest.php
+++ Tests/Form/Extension/ChoiceTypeTest.php
@@ -16,20 +16,38 @@ class ChoiceTypeTest extends TypeTestCase
     public function testChoicesOptionExpectsArray()
     {
         $valueBinded = 3;
-        $this->checkViewVars($valueBinded, $this->choices[$valueBinded]);
-        $this->checkViewVars($valueBinded, $valueBinded, true);
+        $this->checkViewVars($valueBinded, $this->choices[$valueBinded], array(
+            'choices' => $this->choices,
+            'is_active' => false,
+        ));
+
+        $this->checkViewVars($valueBinded, $valueBinded, array(
+            'choices' => $this->choices,
+            'is_active' => true,
+        ));
+
+        $this->checkViewVars($valueBinded, array(false, false, true), array(
+            'choices' => $this->choices,
+            'is_active' => false,
+            'expanded' => true,
+            'multiple' => false
+        ));
+
+        $this->checkViewVars($valueBinded, array(false, false, true), array(
+            'choices' => $this->choices,
+            'is_active' => true,
+            'expanded' => true,
+            'multiple' => false
+        ));
     }
 
-    protected function checkViewVars($valueBinded, $value, $isActive = false)
+    protected function checkViewVars($valueBinded, $value, array $options)
     {
-        $form = $this->factory->create('choice', null, array(
-            'choices' => $this->choices,
-            'is_active' => $isActive,
-        ));
+        $form = $this->factory->create('choice', null, $options);
         $form->bind($valueBinded);
         $view = $form->createView();
 
-        $this->assertEquals($value, $view->vars['value']);
+        $this->assertEquals($value, $view->vars['value'], "Invalid display choix field value");
     }
 
     protected function getExtensions()

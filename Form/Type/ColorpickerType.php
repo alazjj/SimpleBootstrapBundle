46e6f337985d127b277d5e62f3904277f9eae1f9
aRn0D <arnaud.langlade@gmail.com>
2013-04-20 22:55:47 +0200

    Update comments

--- Form/Type/ColorpickerType.php
+++ Form/Type/ColorpickerType.php
@@ -7,32 +7,31 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class ColorpickerType extends AbstractType
 {
-
+    /**
+     * {@inheritdoc}
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(
             array(
                 'widget' => 'single_text',
                 'attr'   => array(
-                    'class'           => 'input-small',
-//                    'pickerContainer' => array(
-//                        'class'  => 'input-append colorpicker',
-//                        'format' => 'hex'
-//                    )
+                    'class' => 'input-small',
                 ),
             )
         );
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getParent()
     {
-        return 'field';
+        return 'form';
     }
 
     /**
-     * Returns the name of this type.
-     *
-     * @return string The name of this type
+     * {@inheritdoc}
      */
     public function getName()
     {

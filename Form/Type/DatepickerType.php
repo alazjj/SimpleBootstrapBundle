3a1b43733444ac61874aa7b8eed8970d43ec4b62
aRn0D <arnaud.langlade@gmail.com>
2013-04-21 19:08:46 +0200

    Add @deprecated annotation

--- Form/Type/DatepickerType.php
+++ Form/Type/DatepickerType.php
@@ -7,6 +7,8 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * @deprecated deprecated since version 1.2
  */
 
 namespace Alazjj\SimpleBootstrapBundle\Form\Type;

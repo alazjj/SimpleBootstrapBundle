5833713fb2fb268d14425f0af7113d9b4b9041a0
aRn0D <arnaud.langlade@gmail.com>
2013-04-21 19:04:17 +0200

    Add licence and copyright mention.

--- DependencyInjection/AlazjjSimpleBootstrapExtension.php
+++ DependencyInjection/AlazjjSimpleBootstrapExtension.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the SimpleBootstrap Bundle.
+ *
+ * @author aRn0D (Arnaud Langlade) <arn0d.dev@gmail.com>
+ * @author Julien Janvier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Alazjj\SimpleBootstrapBundle\DependencyInjection;
 
